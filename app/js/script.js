1e6cd82b9165596a7a24f799c3d3f5b0c98a5f60
alexeyserduk123 <alexeyserduk1@ukr.net>
2017-11-03 15:00:26 +0200

    try

---
+++ app/js/script.js
@@ -0,0 +1,11 @@
+
+$('.owl_carousel').owlCarousel({
+  singleItem: true,
+  navigation: true,
+  navigationText: ['<img src="img/strelka1.png" alt="">','<img src="img/strelka2.png" alt="">']
+ });
+$('.owl_carousel_1').owlCarousel({
+  singleItem: true,
+  navigation: true,
+  navigationText: ['<img src="img/strelka1.png" alt="">','<img src="img/strelka2.png" alt="">']
+ });
